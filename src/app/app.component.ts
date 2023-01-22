e79196583bc02ef62421ec229be695530f4bddbd
Rajesh Verma <info.mrverma01@gmail.com>
2023-01-22 22:02:31 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'url-opener';
+
+  public userInput  : string = '';
+  public urlArray : Array<any> = [];
+  public count : number = 0
+
+  public stringToArray(){
+    this.urlArray = []
+    let array = this.userInput.split("\n");
+    array.forEach(element=>{
+      if(element){
+        this.urlArray.push(element)
+      }
+    })
+    this.count = this.urlArray.length
+  }
+
+  public openUrl(){
+    // convert string to array
+    if(this.urlArray = []){
+      this.urlArray = this.userInput.split("\n");
+    }
+    try {
+      //  open each url in new tab
+       this.urlArray.forEach((element : string) => {
+       if(element.includes("http:")|| element.includes("https:")){
+        window.open(element , '_blank')
+       }
+    });
+    } catch (error) {
+      alert(error)
+    }
+  }
+
 }
